81a512605f7de6b234e9d43810a14add4d5279fe
Suniti13 <sunitigaonkar14@gmail.com>
2019-09-28 02:23:26 +0530

    update1

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, {});
+    var elems1 = document.querySelectorAll('.parallax');
+    var instances = M.Parallax.init(elems1, {});
   });
 
 
